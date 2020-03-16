6ce7feea22db7a5f4c8076aabeba1615e62545df
henry <henry.zhang90@gmail.com>
2020-03-15 18:02:39 -0700

    adsfas

--- src/main/java/edu/ucsb/cs56/w20/lab07/services/MembershipService.java
+++ src/main/java/edu/ucsb/cs56/w20/lab07/services/MembershipService.java
@@ -3,7 +3,9 @@ package edu.ucsb.cs56.w20.lab07.services;
 import java.util.List;
 
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface MembershipService {
 
     /**
